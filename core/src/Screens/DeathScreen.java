7f178af5e6c19e15b9c9cf8f7390c3d4a4ed5937
OwenTishenkel <owen.tishenkel.2019@student.lhschools.org>
2018-09-25 09:21:22 -0400

    Added Death Screen and modified Main Menu

--- core/src/Screens/DeathScreen.java
+++ core/src/Screens/DeathScreen.java
@@ -7,13 +7,13 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class MainmenuScreen implements Screen {
-    private static final String TAG= MainmenuScreen.class.getSimpleName() ;
+public class DeathScreen implements Screen {
+    private static final String TAG= DeathScreen.class.getSimpleName() ;
     private SpriteBatch batch;
     private Texture img;
     private ArchaicInsurrection game;
 
-    public MainmenuScreen(ArchaicInsurrection game, SpriteBatch batch) {
+    public DeathScreen(ArchaicInsurrection game, SpriteBatch batch) {
         this.batch = batch;
         this.game = game;
     }
@@ -21,38 +21,39 @@ public class MainmenuScreen implements Screen {
 
     @Override
     public void show() {
-        Gdx.app.log(TAG, "In SHOW method of MainmenuScreen class");
+        Gdx.app.log(TAG, "In SHOW method of DeathScreen class");
     }
 
     @Override
     public void render(float delta) {
-        Gdx.app.log(TAG, "In RENDER method of MainmenuScreen class");
-        Gdx.gl.glClearColor(5, 0, 90, 1);
+        Gdx.app.log(TAG, "In RENDER method of DeathScreen class");
+        Gdx.gl.glClearColor(1, 22, 100, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
     }
 
     @Override
     public void resize(int width, int height) {
-        Gdx.app.log(TAG, "In RESIZE method of MainmenuScreen class");
+        Gdx.app.log(TAG, "In RESIZE method of DeathScreen class");
     }
 
     @Override
     public void pause() {
-        Gdx.app.log(TAG, "In PAUSE method of MainmenuScreen class");
+        Gdx.app.log(TAG, "In PAUSE method of DeathScreen class");
     }
 
     @Override
     public void resume() {
-        Gdx.app.log(TAG, "In RESUME method of MainmenuScreen class");
+        Gdx.app.log(TAG, "In RESUME method of DeathScreen class");
     }
 
     @Override
     public void hide() {
-        Gdx.app.log(TAG, "In HIDE method of MainmenuScreen class");
+        Gdx.app.log(TAG, "In HIDE method of DeathScreen class");
     }
 
     @Override
     public void dispose() {
-        Gdx.app.log(TAG, "In DISPOSE method of MainmenuScreen class");
+        Gdx.app.log(TAG, "In DISPOSE method of DeathScreen class");
     }
 }
+
