ce39b33485cbaef18a81523b701dc05b80a994c6
OwenTishenkel <owen.tishenkel.2019@student.lhschools.org>
2018-10-29 10:00:36 -0400

    Ashley setup

--- core/src/Managers/EntityManager.java
+++ core/src/Managers/EntityManager.java
@@ -4,9 +4,12 @@ import com.archaicinsurrection.ArchaicInsurrection;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.PooledEngine;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
+import Components.BodyComponent;
 import Helpers.BodyGenerator;
+import Helpers.Figures;
 
 public class EntityManager {
 
@@ -32,8 +35,31 @@ public class EntityManager {
 
     }
 
-    private Entity addBodyComponent() {
-        
+    private Entity addBodyComponent(Entity entity,String entityName,int x, int y) {
+        BodyComponent bodyComponent = engine.createComponent(BodyComponent.class);
+        FixtureDef fdef = new FixtureDef();
+    //method used to build the body
+        switch(entityName) {
+            case"player":
+                fdef.filter.categoryBits= Figures.PLAYER;
+
+            case"tier1":
+
+            case"mini1":
+
+            case"boss":
+
+
+        }
+
+
+
+
+
+
+
+
+        entity.add(bodyComponent);
     }
 
 }
