af2fa2b449fa30516d30b0a5a8b8b98bf4ba483b
Unknown <owen.tishenkel.2019@student.lhschools.org>
2019-03-07 09:42:55 -0500

    Asset Animations 2

---
+++ android/assets/ArchaicTileSet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="ArchaicTileSet" tilewidth="16" tileheight="16" tilecount="144" columns="12">
+ <image source="BasicGimptiles.png" width="200" height="200"/>
+</tileset>
