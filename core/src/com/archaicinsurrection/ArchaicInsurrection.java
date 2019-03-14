3db3be16a05f8655819784197bfdbc20b5baae4a
Unknown <owen.tishenkel.2019@student.lhschools.org>
2019-03-14 10:28:48 -0400

    Asset Manager setup 1

--- core/src/com/archaicinsurrection/ArchaicInsurrection.java
+++ core/src/com/archaicinsurrection/ArchaicInsurrection.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.AssetManager;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -18,6 +19,8 @@ import Screens.MainmenuScreen;
 public class ArchaicInsurrection extends Game {
     private static final String TAG = ArchaicInsurrection.class.getSimpleName();
     SpriteBatch batch;
+    private AssetManager
+
     Texture img;
     private LoadingScreen loadingscreen;
     private MainmenuScreen mainmenuscreen;
