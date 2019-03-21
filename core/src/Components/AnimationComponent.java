b02ae1b60f47aa18564bece47e0ff97bdc90d3a5
Unknown <owen.tishenkel.2019@student.lhschools.org>
2019-03-21 10:26:47 -0400

    Creating Animation Component
    8:19

---
+++ core/src/Components/AnimationComponent.java
@@ -0,0 +1,29 @@
+package Components;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.utils.ArrayMap;
+import com.badlogic.gdx.utils.Pool.Poolable;
+
+public class AnimationComponent implements Component, Poolable {
+
+    public enum ANIMATIONSTATE{
+        UP,DOWN,LEFT,RIGHT,HIT,ATTACKING,PREPARING_TO_ATTACK,DYING,IMMOBILE
+    }
+    private float time=0.0f;
+    private boolean isLooping=false;
+
+    private ArrayMap<ANIMATIONSTATE,Animation> animations=new ArrayMap<>();
+
+    public AnimationComponent addAnimation(ANIMATIONSTATE state,Animation animation) {
+        this.animations.put(state,animation);
+        return this;
+    }
+
+    
+
+    @Override
+    public void reset() {
+
+    }
+}
