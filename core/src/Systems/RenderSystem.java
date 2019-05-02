dc599d93455374c03cdf285b938c4a9f366d5e06
Unknown <owen.tishenkel.2019@student.lhschools.org>
2019-05-02 10:26:20 -0400

    Rendering System
    13:01

--- core/src/Systems/RenderSystem.java
+++ core/src/Systems/RenderSystem.java
@@ -19,12 +19,22 @@ public class RenderSystem extends IteratingSystem {
         super(Family.all(RenderableComponent.class).get());
         this.batch=batch;
         this.camera=camera;
+        bodiesQueue= new Array<>();
 
     }
 
+    @Override
+    public void update(float deltaTime) {
+        super.update(deltaTime);
+        batch.setProjectionMatrix(camera.combined);
+        batch.begin();
+        batch.end();
+        bodiesQueue.clear();
+    }
 
     @Override
     protected void processEntity(Entity entity, float deltaTime) {
+        bodiesQueue.add(entity);
 
     }
 }
